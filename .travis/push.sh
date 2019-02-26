048df004854d600cd867afd8a639b96d3a55acbe
Simon Pieters <zcorpan@gmail.com>
2019-02-26 20:55:24 +0100

    Fix more errors in push.sh and try to make it exit 1 on failures

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "travis@travis-ci.org" || exit 1
+  git config --global user.name "Travis CI" || exit 1
 }
 
 commit_files() {
-  git checkout -b leanpub
-  git add manuscript/*.md
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git checkout -b leanpub || exit 1
+  git add manuscript/*.md || exit 1
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER" || exit 1
 }
 
 upload_files() {
-  git remote add zcorpan https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream zcorpan leanpub
+  git remote add zcorpan https://${GH_TOKEN}@github.com/zcorpan/html-parser-book.git > /dev/null 2>&1 || exit 1
+  git push --quiet --set-upstream zcorpan leanpub || exit 1
 }
 
-setup_git || exit 1
-commit_files || exit 1
-upload_files || exit 1
+setup_git
+commit_files
+upload_files
