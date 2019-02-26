129ee1f2c16b57c635ba7fc9ccf762c7bb60e447
Simon Pieters <zcorpan@gmail.com>
2019-02-26 16:58:09 +0100

    Try using sed instead of perl

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 replace() {
-  perl -p -i -e "s/$1/$2/g" "$3"
+  # https://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux#comment95713433_51060063
+  sedi=(-i) && [ "$(uname)" == "Darwin" ] && sedi=(-i '') sed "${sedi[@]}" -e "s/$1/$2/" "$3"
 }
 
 find manuscript/ -type f -name "*.md" | while read -r file; do
-  replace '```html' '{line-numbers=off,lang=html}\n~~~~~~~~' "$file"
-  replace '```js' '{line-numbers=off,lang=js}\n~~~~~~~~' "$file"
+  replace '```html' '{line-numbers=off,lang=html}\
+~~~~~~~~' "$file"
+  replace '```js' '{line-numbers=off,lang=js}\
+~~~~~~~~' "$file"
   replace '```' '~~~~~~~~' "$file"
 done
