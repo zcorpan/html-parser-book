0c147655de347b7899a8119bd2db0f50bc597b48
Simon Pieters <zcorpan@gmail.com>
2019-03-04 01:19:31 +0100

    Try to fix substitution error

--- build-web.sh
+++ build-web.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 find manuscript -name "*.md" | while read -r file; do
-  node_modules/showdown/bin/showdown.js makehtml --quiet --ghCompatibleHeaderId --tables --ghCodeBlocks -i "$file" -o "${file/.*/}.html" | exit 1
+  node_modules/showdown/bin/showdown.js makehtml --quiet --ghCompatibleHeaderId --tables --ghCodeBlocks -i "$file" -o "${file/.*}.html" | exit 1
 done
