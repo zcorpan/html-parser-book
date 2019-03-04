6c709d912b1e167ffc21f0f48dcb66ac979a7ce0
Simon Pieters <zcorpan@gmail.com>
2019-03-04 02:53:32 +0100

    Add a header to generated HTML files; add index.html

--- build-web.sh
+++ build-web.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+html="<!doctype html>
+<meta charset=utf-8>
+<meta name=viewport content='width=device-width,initial-scale=1'>
+<link rel=stylesheet href=style.css>
+"
+
 find manuscript -name "*.md" | while read -r file; do
-  showdown makehtml --quiet --ghCompatibleHeaderId --tables --ghCodeBlocks -i "$file" -o "${file/.*}.html" || exit 1
+  title="${file/.*}"
+  title="${title/*\//}"
+  title="${title/-/ }"
+  title="${title/dom /DOM }"
+  title=`echo ${title:0:1} | tr  '[a-z]' '[A-Z]'`${title:1}
+  echo "$html<title>$title - The HTML Parser Book</title>" > "${file/.*}.html" || exit 1
+  showdown makehtml --quiet --ghCompatibleHeaderId --tables --ghCodeBlocks -i "$file" >> "${file/.*}.html" || exit 1
 done
