2ad2c35eb909645a46010d1b3c2ef278ac02038a
Simon Pieters <zcorpan@gmail.com>
2019-03-04 01:42:27 +0100

    Oops, silly mistake in bash

--- build-web.sh
+++ build-web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 find manuscript -name "*.md" | while read -r file; do
-  showdown makehtml --quiet --ghCompatibleHeaderId --tables --ghCodeBlocks -i "$file" -o "${file/.*}.html" | exit 1
+  showdown makehtml --quiet --ghCompatibleHeaderId --tables --ghCodeBlocks -i "$file" -o "${file/.*}.html" || exit 1
 done
